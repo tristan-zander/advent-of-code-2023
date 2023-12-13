62006bbc9a9fec55f5d788f81a3d7094f31ae84c
Tristan Zander <tristan.zander@ncino.com>
2023-12-13 13:01:42 -0500

    Day 12 ACTUALLY WORKS!!! (IMPOSSIBLE)

--- src/day_twelve.rs
+++ src/day_twelve.rs
@@ -1,7 +1,5 @@
-use std::{sync::Arc, time::Instant};
-
 use dashmap::DashMap;
-use itertools::{repeat_n, Itertools};
+use itertools::Itertools;
 use rayon::prelude::*;
 
 use crate::Args;
@@ -9,7 +7,7 @@ use crate::Args;
 const FILE_CONTENTS: &'static str = include_str!("../inputs/day_twelve.txt");
 
 lazy_static::lazy_static! {
-    static ref PERMUTATIONS: DashMap<usize, Arc<Vec<String>>> = DashMap::new();
+    static ref MEMOIZED: DashMap<u64, usize> = DashMap::new();
 }
 
 fn read_input() -> Vec<(Vec<u8>, Vec<u8>)> {
@@ -28,111 +26,86 @@ fn read_input() -> Vec<(Vec<u8>, Vec<u8>)> {
         .collect_vec()
 }
 
-fn find_holes(bytes: &[u8]) -> impl Iterator<Item = usize> + '_ {
-    bytes.iter().enumerate().filter_map(|(i, byte)| {
-        if byte == &b'?' {
-            return Some(i);
-        } else {
-            return None;
-        }
-    })
+fn calc_memoized(bytes: &[u8], groups: &[u8]) -> usize {
+    use std::hash::Hasher;
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    hasher.write(bytes);
+    hasher.write_u8(0xFF);
+    hasher.write(groups);
+    let hash = hasher.finish();
+    if let Some(res) = MEMOIZED.get(&hash) {
+        return res.clone();
+    }
+
+    let res = calc(bytes, groups);
+    MEMOIZED.insert(hash, res);
+    return res;
 }
 
-fn hole_groups(holes: &[usize]) -> Vec<(usize, usize)> {
-    let mut holes = holes.iter().cloned();
-    let mut res = Vec::new();
-    let mut prev = holes.next().unwrap();
-    let mut buf = vec![prev];
-
-    while let Some(hole_idx) = holes.next() {
-        if hole_idx - prev != 1 {
-            let start_idx = buf[0];
-            let len = buf.len();
-            res.push((start_idx, len));
-            buf.clear();
-        }
-
-        buf.push(hole_idx);
-        prev = hole_idx;
-    }
-
-    if buf.len() != 0 {
-        let start_idx = buf[0];
-        let len = buf.len();
-        res.push((start_idx, len));
-        buf.clear();
-    }
-
+fn dot(bytes: &[u8], groups: &[u8]) -> usize {
+    let res = calc_memoized(&bytes[1..], groups);
+    let _do_not_optimize = res + 1 - 1;
     res
 }
 
-fn is_combination(buf: &str, seq: &Vec<u8>) -> bool {
-    let groups = buf
-        .split('.')
-        .filter_map(|grp| {
-            if grp.len() == 0 {
-                return None;
-            }
-            return Some(grp.len() as u8);
-        })
-        .collect_vec();
+fn hash(bytes: &[u8], groups: &[u8]) -> usize {
+    let curr_group_size = groups[0];
+    let group: &[u8] = &bytes[..curr_group_size as usize];
+    if group.iter().any(|c| c == &b'.') {
+        // It does not match the next group.
+        return 0;
+    }
+    let next_slice = &bytes[curr_group_size as usize..];
+    let next_group = &groups[1..];
 
-    if groups.len() != seq.len() {
-        return false;
+    if next_slice.len() == 0 {
+        return calc_memoized(next_slice, next_group);
     }
 
-    groups
-        .into_iter()
-        .zip(seq.iter())
-        .all(|(left, right)| left == *right)
+    if next_slice[0] == b'#' {
+        // There needs to be a boundary between groups.
+        return 0;
+    }
+
+    let res = calc_memoized(&next_slice[1..], next_group);
+    let _do_not_optimize = res + 1 - 1;
+    res
 }
 
-fn get_permutations(len: usize) -> Arc<Vec<String>> {
-    if let Some(perm) = PERMUTATIONS.get(&len) {
-        return perm.clone();
+fn calc(bytes: &[u8], groups: &[u8]) -> usize {
+    if groups.len() == 0 {
+        if bytes.contains(&b'#') {
+            // There are no more groups but there are more hashes.
+            return 0;
+        }
+
+        // There are no more groups and no more hashes.
+        return 1;
     }
 
-    let perms = repeat_n([".", "#"], len)
-        .multi_cartesian_product()
-        .map(|perms| perms.join(""))
-        .collect_vec();
+    if bytes.len() == 0 {
+        return 0;
+    }
 
-    PERMUTATIONS.insert(len, Arc::new(perms));
-    let perms = PERMUTATIONS.get(&len).unwrap();
+    if bytes.len() < groups.iter().cloned().reduce(|acc, x| acc + 1 + x).unwrap() as usize {
+        // The length of the string is too small to contain the groups
+        return 0;
+    }
 
-    perms.clone()
+    let first_byte = bytes[0];
+
+    let res = match first_byte {
+        b'.' => dot(bytes, groups),
+        b'#' => hash(bytes, groups),
+        b'?' => dot(bytes, groups) + hash(bytes, groups),
+        _ => unreachable!(),
+    };
+    let _do_not_optimize = res + 1 - 1;
+    res
 }
 
 fn solve(bytes: Vec<u8>, sequence: Vec<u8>) -> usize {
-    let holes = find_holes(&bytes).collect_vec();
-    let groups = hole_groups(&holes);
-
-    let permutation_ptrs = groups
-        .iter()
-        .map(|(_start, len)| {
-            let permutations = get_permutations(*len);
-            permutations
-        })
-        .collect_vec();
-
-    let unique_combos_per_group = permutation_ptrs
-        .iter()
-        .map(|p| p.as_slice())
-        .multi_cartesian_product();
-
-    let mut final_string = String::from_utf8(bytes.clone()).unwrap();
-    let mut sum = 0;
-    for group in unique_combos_per_group {
-        for (replacement, (start, len)) in group.into_iter().zip(groups.iter().cloned()) {
-            final_string.replace_range(start..start + len, &replacement);
-        }
-
-        if is_combination(&final_string, &sequence) {
-            sum += 1;
-        }
-    }
-
-    sum
+    calc_memoized(&bytes, &sequence)
 }
 
 pub fn part_one(_args: Args) {
@@ -147,18 +120,23 @@ pub fn part_one(_args: Args) {
 pub fn part_two(_args: Args) {
     let input = read_input()
         .into_iter()
-        .map(|(bytes, seq)| (bytes.repeat(5), seq.repeat(5)))
+        .map(|(bytes, seq)| ((0..5).map(move |_i| bytes.clone()), seq.repeat(5)))
+        .map(|(bytes, seq)| {
+            (
+                bytes
+                    .map(|part| String::from_utf8(part).unwrap())
+                    .into_iter()
+                    .join("?")
+                    .bytes()
+                    .collect_vec(),
+                seq,
+            )
+        })
         .collect_vec();
+
     let sum = input
         .into_par_iter()
-        .enumerate()
-        .map(|(i, (bytes, sequence))| {
-            println!("Solving {}", i);
-            let start = Instant::now();
-            let solved = solve(bytes, sequence);
-            println!("{} solved in {}", i, (start - Instant::now()).as_millis());
-            solved
-        })
+        .map(|(bytes, sequence)| solve(bytes, sequence))
         .sum::<usize>();
     println!("Sum: {}", sum);
 }
